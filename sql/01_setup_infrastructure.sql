37d638f15ddbc06240703f66983914d91c704cf8
ginobaltazar7 <34085665+ginobaltazar7@users.noreply.github.com>
2026-09-11 17:07:48 +0900

    link git repo in sql/01_setup_infrastructure.sql

--- sql/01_setup_infrastructure.sql
+++ sql/01_setup_infrastructure.sql
@@ -1,4 +1,5 @@
 -- Description: Provisions database tiers, external network rules for API access, and the serverless compute pool required to host containerized workloads natively inside Snowflake.
+USE ROLE ACCOUNTADMIN;
 CREATE DATABASE IF NOT EXISTS SPORTS_ANALYTICS_DB;
 CREATE SCHEMA IF NOT EXISTS SPORTS_ANALYTICS_DB.RAW_BRONZE;
 CREATE SCHEMA IF NOT EXISTS SPORTS_ANALYTICS_DB.SILVER;
@@ -21,6 +22,21 @@ CREATE COMPUTE POOL IF NOT EXISTS dbt_compute_pool
   MAX_NODES = 1
   INSTANCE_FAMILY = CPU_X64_XS
   AUTO_RESUME = TRUE;
+
+-- Establish Secure GitHub API Integration
+CREATE OR REPLACE API INTEGRATION github_api_integration
+  API_PROVIDER = git_https_api
+  API_ALLOWED_PREFIXES = ('https://github.com/ginobaltazar7/hyrox-tieredsla-pipeline')
+  ENABLED = TRUE;
+
+-- Register Native Git Repository Stage
+CREATE OR REPLACE GIT REPOSITORY SPORTS_ANALYTICS_DB.RAW_BRONZE.hyrox_repo
+  API_INTEGRATION = github_api_integration
+  ORIGIN = 'https://github.com/ginobaltazar7/hyrox-tieredsla-pipeline';
+
+-- Fetch Code and Verify Staging Files
+ALTER GIT REPOSITORY SPORTS_ANALYTICS_DB.RAW_BRONZE.hyrox_repo FETCH;
+LS @SPORTS_ANALYTICS_DB.RAW_BRONZE.hyrox_repo/branches/main;  
 
 -- Egress network rule and external access integration for pyrox-client package retrieval
 -- Works only with Snowflake accounts that have External Network Access enabled and configured and not a trial account. If you are using a trial account, you can skip this step and manually install the pyrox-client package in your local environment.
